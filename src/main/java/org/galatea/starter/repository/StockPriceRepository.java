4a2c1ce075b439d24cf19041ca37e103bf293363
Gabe Scott <gabe.scott@galatea-associates.com>
2020-02-05 09:59:01 -0500

    checkstyle fix

--- src/main/java/org/galatea/starter/repository/StockPriceRepository.java
+++ src/main/java/org/galatea/starter/repository/StockPriceRepository.java
@@ -15,7 +15,7 @@ public interface StockPriceRepository extends JpaRepository<StockDay, Integer> {
    * Given a stock id and a starting date, retrieve all stock data for this stock on days after the
    * given date. does what it says on the tin.
    */
-  @Query(value = "select * from price where stock_id = ?1 and trade_date >= ?2 ", nativeQuery = true)
+  @Query(value = "select * from price where stock_id = ?1 and trade_date >= ?2", nativeQuery = true)
   List<StockDay> findStockHistoryById(final int stockId,
       @NonNull final OffsetDateTime date);
 
